a002e7e1daaf6f6413d8b0382598e45abc5821ab
Cem <cemayder97@gmail.com>
2017-09-08 22:27:30 +0300

    Initial commit

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config              = {}
+Config.MaxDistance = 1.5
+
+Config.Sitable = {
+	--Only verticalOffset works right now!
+	--all scenarios: pastebin.com/6mrYTdQv
+	{prop = 'prop_bench_01b', scenario = 'PROP_HUMAN_SEAT_BENCH', verticalOffset = -0.5, forwardOffset = 0.0, leftOffset = 0.0},
+	{prop = 'prop_bench_09', scenario = 'PROP_HUMAN_SEAT_BENCH', verticalOffset = -0.7, forwardOffset = 0.0, leftOffset = 0.0}
+}
